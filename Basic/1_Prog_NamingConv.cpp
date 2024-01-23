92c09a7629208cb76f8df4403d5f30e3414b334d
YOGESH17IITM <yogeshwaranrubin@gmail.com>
2024-01-23 23:08:32 +0530

    Updated comment

--- Basic/1_Prog_NamingConv.cpp
+++ Basic/1_Prog_NamingConv.cpp
@@ -1,22 +1,22 @@
 /*
-*	Topic:	Naming Convensions (name mangling)
-*	Explanation:
-*		1)	In CPP, name mangling is supported.
-*			For example, in the below function,
-*				'func_cpp' is created as "?func_cpp@@YAXXZ" and 
-*				'func_cpp(int)' is created as "?func_cpp@@YAXH@Z"
-* 
-*		2)	In "C", name mangline is not supported.
-*			For instance, thee function "func_c" is created as "func_c" only
-* 
-*	Note:
-*		To check, one can do the following:
-*		- Right Click on VS Solution > C/C++ > Output Files > Assembler Output --> Set Assembly with Source Code (/FAs)
-*		- Then, run with Debug mode. Check the file: x<64/86 arch>/Debug/<filename>.ASM
-*		- Search for string "func_cpp" and "func_c"
-*/
+ *	Topic:	Naming Convensions (name mangling)
+ *	Explanation:
+ *		1)	In CPP, name mangling is supported.
+ *			For example, in the below function,
+ *				'func_cpp' is created as "?func_cpp@@YAXXZ" and
+ *				'func_cpp(int)' is created as "?func_cpp@@YAXH@Z"
+ *
+ *		2)	In "C", name mangline is not supported.
+ *			For instance, the function "func_c" is created as "func_c" only
+ *
+ *	Note:
+ *		To check, one can do the following:
+ *		- Right Click on VS Solution > C/C++ > Output Files > Assembler Output --> Set Assembly with Source Code (/FAs)
+ *		- Then, run with Debug mode. Check the file: x<64/86 arch>/Debug/<filename>.ASM
+ *		- Search for string "func_cpp" and "func_c"
+ */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 /*************** CPP FUNC *********************/
@@ -24,27 +24,29 @@ void func_cpp()
 // internally it interprets as given below:
 // "extern "C++" void func_C++()
 {
-	cout << "func_C++() \n";
+    cout << "func_C++() \n";
 }
 
-void func_cpp(int) // function overloaded 
+void func_cpp(int) // function overloaded
 {
-	cout << "func_C++(int) \n";
+    cout << "func_C++(int) \n";
 }
 /*******************************************/
 
 /***************** C FUNC *******************/
 extern "C" void func_c()
 {
-	printf("Hello I am C %d", 1);
+    printf("Hello I am C %d", 1);
 }
+// Note: overloading NOT allowed for fucntion with 'extern "C" linkage'
+// for example, extern "C" void func_c(int) --> NOT allowed. Will give compilation error.
 /*******************************************/
 
 int main()
 { // Prolog
-	// Body
-	func_cpp();
-	func_c();
-	// Epilog
-	return 0; // return type	
+    // Body
+    func_cpp();
+    func_c();
+    // Epilog
+    return 0; // return type
 }
