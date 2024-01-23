23b289aa80f8e6c5e8c8f043888c2679dcc67b90
YOGESH17IITM <yogeshwaranrubin@gmail.com>
2024-01-23 23:29:20 +0530

    Updated comment

--- Basic/3_Prog_Default_Ctor.cpp
+++ Basic/3_Prog_Default_Ctor.cpp
@@ -1,54 +1,55 @@
 /*
-*	TOPIC:
-*		C'tor synthesization:
-* 
-*	DISCUSSION:
-*		- There is misconception that default c'tor is always will be synthesized by the compiler which is NOT true.
-*		- It will be synthesized only when needed as shown in the examples below
-*/
-#include<iostream>
+ *	TOPIC:
+ *		C'tor synthesization:
+ *
+ *	DISCUSSION:
+ *		- There is misconception that default c'tor is always will be synthesized by the compiler which is NOT true.
+ *		- It will be synthesized only when needed as shown in the examples below
+ */
+#include <iostream>
 using namespace std;
 
 // Case: A
 class Base
 {
 public:
-	Base() {} // prolog - construction happens here
+    Base() {} // prolog - construction happens here
 };
 
 class Derived : public Base
 {
 public:
-	// Here the Derived class c'tor will be synthesized in order to call the Base class c'tor. 
-	// Refer the line in .asm file: ??0Derived@@QEAA@XZ				; Derived::Derived
+    // Here the Derived class c'tor will be synthesized in order to call the Base class c'tor.
+    // Refer the line in .asm file: ??0Derived@@QEAA@XZ				; Derived::Derived
 
-	// Things to try:
-	// Try keeping "Derived()" and comment out "Base()", check again.
-	// Default Base() will not be created in this case. Verify from .asm file
+    // Things to try:
+    // Try keeping "Derived()" and comment out "Base()", check again.
+    // Default Base() will not be created in this case. Verify from .asm file
 };
 
 // Case: B
 class Composite
 {
-	Base objBase;
+    Base objBase;
+
 public:
-	// Here the Derived class c'tor will be synthesized in order to call the Base class c'tor. 
-	// Refer the line in .asm file: ??0Composite@@QEAA@XZ				; Composite::Composite
+    // Here the composite class c'tor will be synthesized in order to call the Base class c'tor.
+    // Refer the line in .asm file: ??0Composite@@QEAA@XZ				; Composite::Composite
 };
 
 // Case: C
 class ClassWithVirtualFunc
 {
 public:
-	// C'tor will be synthesized inorder to set/map the vPtr to appropriate vTable.
-	// Refer the line in .asm file: ??0ClassWithVirtualFunc@@QEAA@XZ		; ClassWithVirtualFunc::ClassWithVirtualFunc
-	virtual void SomeFunc() {}
+    // C'tor will be synthesized inorder to set/map the vPtr to appropriate vTable.
+    // Refer the line in .asm file: ??0ClassWithVirtualFunc@@QEAA@XZ		; ClassWithVirtualFunc::ClassWithVirtualFunc
+    virtual void SomeFunc() {}
 };
 
 int main()
 {
-	Derived objDer;
-	Composite objComp;
-	ClassWithVirtualFunc objVirClass;
-	return 0;
+    Derived objDer;
+    Composite objComp;
+    ClassWithVirtualFunc objVirClass;
+    return 0;
 }
