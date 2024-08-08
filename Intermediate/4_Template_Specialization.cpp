b5681af36512f67d6179c4791bc2b1ceb9175505
YOGESH17IITM <yogeshwaranrubin@gmail.com>
2024-08-08 18:47:31 +0530

    Updated comment

--- Intermediate/4_Template_Specialization.cpp
+++ Intermediate/4_Template_Specialization.cpp
@@ -1,32 +1,33 @@
 /*
-* TOPIC:
-*	- Class template specialization
-*	- Constexpr -> used to make the code segment available at compile time.
-*/
-#include<iostream>
+ * TOPIC:
+ *	- Class template specialization
+ *	- Constexpr -> used to make the code segment available at compile time.
+ */
+#include <iostream>
 using namespace std;
 
-template<typename T>
+template <typename T>
 class Log
 {
 public:
-    explicit Log(const T iMsg) {
-        cout << "Your Message is: " << iMsg << endl;
-    }
-};
-
-// Template specialization for pointer type
-template<typename T>
-class Log<T*>	// syntax: class [class-name]<specialization-type>
-{
-public:
-    explicit Log(const T* iMsg)
+    explicit Log(const T iMsg)
     {
         cout << "Your Message is: " << iMsg << endl;
     }
 };
 
-template<int retCode>
+// Template specialization for pointer type
+template <typename T>
+class Log<T *> // syntax: class [class-name]<specialization-type>
+{
+public:
+    explicit Log(const T *iMsg)
+    {
+        cout << "Your Message is: " << iMsg << endl;
+    }
+};
+
+template <int retCode>
 class Result
 {
 public:
@@ -43,20 +44,20 @@ int Func()
 }
 
 constexpr int SomeFunc()
-{    
+{
     return 999;
 }
 
 int main()
 {
     Log<char> objLog('A');
-    Log<char*> objLogPtr("Hello\0");
+    Log<char *> objLogPtr("Hello\0");
 
     const int retCodeA = 0;
     Result<retCodeA> objA;
 
     // Let us consider Func() as template type as it returns 'int' type.
-    // Result<func()> objFunc;      // ERROR: Since Func() executes at run-time but template executes at run-time.
+    // Result<func()> objFunc;      // ERROR: Since Func() executes at run-time but template executes at compile time.
     // To fix this, we can use 'constexpr'
     Result<SomeFunc()> objB;
 
